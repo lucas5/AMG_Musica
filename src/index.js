fe432ac2251dc5de83b20e1b7cc9662fb0fd3b43
Lucas Expedito <lucasexpedito.oli@gmail.com>
2020-06-16 18:48:02 -0300

    Ajustes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const { prefix, token } = require("../config.json");
 const ytdl = require("ytdl-core");
 const client = new Discord.Client();
 const queue = new Map();
